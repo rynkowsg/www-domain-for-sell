7656fdc44e83b7d7d1994dda36d8f00a26d33e7a
Grzegorz Rynkowski <dev@rynkowski.info>
2017-04-09 13:40:41 +0100

    Add basic version of the website

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,30 @@
-// Empty JS for your own code to be here
+(function ($) {
+    "use strict";
+
+    var domainName =  window.location.host;
+
+    function sendEmail() {
+        document.location.href = getMailtoText('greg', 'rynkowski.info');
+    }
+
+    function getMailtoText(name, domain) {
+        var result = "mailto";
+        result += ":" + name + "@" + domain;
+        result += "?" + "subject=" + "RE: %20" + domainName;
+        console.log(result);
+        return result;
+    }
+
+    var mainApp = {
+        main_fun: function () {
+            console.log("main_fun");
+            $('#domain_name').html(domainName);
+            $('#btnSendEmail').on( "click", sendEmail);
+        }
+    };
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
